87476b74e88c41362556eaf0dbc3da982fb03c80
LYL <2062057933@qq.com>
2018-11-14 15:21:15 +0800

    ptr

--- median/TenTest/TenTest/TenTest.cpp
+++ median/TenTest/TenTest/TenTest.cpp
@@ -8,12 +8,61 @@ using namespace std;
 
 int main()
 {
+	int i;
+	double score[5] = { 98, 87, 65, 43, 76 };
+	double * ptr_score;
+	ptr_score = &score[1];
+	ptr_score += 2;
+	cout << *ptr_score << endl;
+	ptr_score -= 3;
+	cout << *ptr_score << endl;
+	/*
+	double score[]{ 11, 22, 33, 44, 55 };
+	double * ptr_score = score;
+
+	//数组——数组名就是数组的首地址
+	//指针的大小一般是4个字节
+	cout << sizeof(score) << '\t' << sizeof(ptr_score) << endl;
+	cout << *(ptr_score + 30) << endl;*/
+
+	/*int i;
+	double score[5]{ 98, 87, 65, 43, 76 };
+	double * ptr_score;
+	ptr_score = score;
+	for (i = 0; i < 5; i++)
+	{
+		cout << *ptr_score++ << endl;
+	}
+	*/
+	/*
+	double score[]{ 11, 22, 33, 44, 55 };
+	double * ptr_score = score;
+
+	//数组——数组名就是数组的首地址
+	//指针的大小一般是4个字节
+	cout << sizeof(score) << '\t' << sizeof(ptr_score) << endl;
+	cout << ptr_score[2] << endl;
+	*/
+
+	/*int num = 108;
+	int& rel_num = num;
+	rel_num = 118;
+	cout << &num << "\t" << &rel_num << endl;
+	*/
+	//等同于如下
+	/*int num = 108;
+	int* rel_num = &num;
+	*rel_num = 118;
+	cout << &num << "\t" << rel_num << endl;*/
+
+	/*
 	double objNum = 3.14;
 	double *ptr_obj = &objNum;
 
 	//cout << boolalpha;
 	void *vptr_obj = &objNum;	//可以指向任意类型
 	cout << (ptr_obj == vptr_obj) << endl;
+	*/
 
 	/*
 	int *ptr1 = nullptr; //等价于int* ptr1 = 0；
